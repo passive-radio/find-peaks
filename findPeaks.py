571c2fb6424f6b9017910f6b2b3da2dae2659f13
passiveradio <srccreator@gmail.com>
2021-10-28 10:17:01 +0900

    [update]

--- findPeaks.py
+++ findPeaks.py
@@ -1,8 +1,14 @@
 class findPeaks(object):
-    def __init__(self, peaks=1) -> None:
+    def __init__(self, peaks, data) -> None:
         self.peaks = int(peaks)
-        pass
+        self.data = data
         
     @property
     def returnPeakNumber(self):
         return int(self.peaks)
+    
+    def reset_range(self, xrange):
+        if xrange != None:
+            start = xrange[0]
+            end = xrange[1]
+            return self.data[start:end]
