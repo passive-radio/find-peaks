f6acb4f1fb950aea1dd001bd52ad3e33150fce35
passiveradio <srccreator@gmail.com>
2021-12-15 14:35:25 +0900

    [update] update interactive.py for optional number of peaks determiantion!

--- example.py
+++ example.py
@@ -1,28 +1,44 @@
+from pandas.core import base
 from findPeaks import read_data, reset_range
-from interactive import plot_test, seeSpectrum
+from interactive import plot_test, seeSpectrum, draw_test
 from bmpToCSV import bmpToCSV
 
 if __name__ == "__main__":
     
     base_url = "sample_data/"
-    endpoint = "sample03.bmp"
-    file_path = base_url + endpoint
+    # endpoint = "sample02.bmp"
+    # file_path = base_url + endpoint
     
-    bmpToCSV(file_path)
+    # bmpToCSV(file_path)
     
-    endpoint = "sample03.csv"
+    # endpoint = "sample02.csv"
     
-    # x_list = []
-    # y_list = []
+    # # x_list = []
+    # # y_list = []
     
-    # with open(base_url+endpoint, 'r', newline='', encoding="utf-8") as file:
-    #     csv_val = csv.reader(file, delimiter=',')
-    #     for row in csv_val:
-    #         x_list.append(float(row[0]))
-    #         y_list.append(float(row[1]))
+    # # with open(base_url+endpoint, 'r', newline='', encoding="utf-8") as file:
+    # #     csv_val = csv.reader(file, delimiter=',')
+    # #     for row in csv_val:
+    # #         x_list.append(float(row[0]))
+    # #         y_list.append(float(row[1]))
     
-    data= read_data(base_url + endpoint, 0, ',')
+    # data= read_data(base_url + endpoint, 0, ',')
     
-    #data = reset_range(data, 260, 330)
+    # # Support optional number of peaks determination with interactive help
+    
+    # """
+    # left click at the top of the peak -> left click at the both edge of the peak -> [guide] Next, click the top of the next peak! -> ...
+    # """
+    
+    # #data = reset_range(data, 260, 330)
+    # plot_test(data)
+    # #seeSpectrum(data)
+    
+    endpoint = "sample.csv"
+    
+    file_path= base_url + endpoint
+    
+    data = draw_test(file_path, mode="wrd")
+    
     plot_test(data)
-    #seeSpectrum(data)
+    
--- interactive.py
+++ interactive.py
@@ -15,9 +15,12 @@ count = 0
 band = 0
 left = 0
 right = 0
+peak_count = 0
 texts = []
 
-def draw_test():
+
+
+def draw_test(file_path, mode):
     
     def motion(event):
         if (event.xdata is  None) or (event.ydata is  None):
@@ -56,13 +59,22 @@ def draw_test():
 
     print(list)
     list = np.array(list)
-
-    path = "test.csv"
-
-    with open(path, 'w', newline='') as file:
-        mywriter = csv.writer(file, delimiter=',')
-        mywriter.writerows(list)
+    
+    try:
+        with open(file_path, 'w', newline='') as file:
+            mywriter = csv.writer(file, delimiter=',')
+            mywriter.writerows(list)
+    except FileNotFoundError as e:
+        print(f"{e}, please verify your file path")
         
+    if mode== "wrd":
+        return read_data(file_path, 0, ",")
+    
+    if mode=="wrp":
+        return file_path
+
+        
+            
 def plot_test(data):
     
     x_list = data.x
@@ -74,14 +86,22 @@ def plot_test(data):
     
     x_peaks = []
     y_peaks = []
+    bands = []
     
-    def motion(event):
+    def button_pressed_motion(event):
         is_click_off = False
         global count
         global band
         global left
         global right
         global texts
+        global peak_count
+        
+        #[event] event.button
+        # value | info
+        #   1   |   left-click
+        #   2   |   mouse wheeling
+        #   3   |   right-click
         
         if (event.xdata is  None) or (event.ydata is  None):
             return
@@ -94,7 +114,7 @@ def plot_test(data):
             x_peaks.append(x)
             y_peaks.append(y)
             plt.title(f"Peak selected! at ({int(x)},{int(y)})")
-            texts.append(ax.text(100, 30, "Next, Please select the bandwidth by clicking the edge of the peak! (left->right)"))
+            #texts.append(ax.text(100, 30, "Next, Please select the bandwidth by clicking the edge of the peak! (left->right)"))
             
         elif event.button == 1 and count == 1:
             left = event.xdata
@@ -103,23 +123,29 @@ def plot_test(data):
         elif event.button == 1 and count == 2:
             right = event.xdata
             band = abs(left- right)
-            texts[0].remove()
+            bands.append(band)
+            #texts[peak_count].remove()
             plt.title(f"Bandwidth selected!: {band}")
             ax.text(100, 30, "You can now close this window!")
+        
+        if event.button == 3 and count == 2:
+            plt.title(f"now selected nother peak!")
+            peak_count += 1
+            count = 0
             
         plt.draw()
     
     fig = plt.figure()
     ax = fig.add_subplot()
     plt.title("Please click the top of the peak! :)")
-    plt.connect('button_press_event', motion)
+    plt.connect('button_press_event', button_pressed_motion)
     plt.scatter(x_list, y_list, s=2)
     plt.show()
     
     print("-"*30)
     print("Pointed peak!")
     print("x y bandwidth")
-    print(x_peaks[0], y_peaks[0], band)
+    print(x_peaks[0], y_peaks[0], bands[0])
     print("-"*30)
         
     #data = reset_range(data_origin, 1600)
@@ -128,7 +154,12 @@ def plot_test(data):
     #初期値のリストを作成
     #[amp,ctr,wid]
     guess = []
-    guess.append([x_peaks[0], y_peaks[0], band])
+    for i in range(len(x_peaks)):
+        try:
+            guess.append([x_peaks[i], y_peaks[i], bands[i]])
+        except Exception as e:
+            print(e)
+            pass
 
     #バックグラウンドの初期値
     background = 0
@@ -173,5 +204,6 @@ if __name__ == "__main__":
     #         y_list.append(float(row[1]))
     
     data= read_data(base_url + endpoint, 0, ',')
+    
         
     plot_test(data)
